bf9e0d6e469b5e221d7a805becfa084f41cbd3a5
Matin HosseinZadeh <matin.hosseinzadeh06@gmail.com>
2025-03-03 01:50:04 +0330

    Revert "apply VAZIR font to labels"

--- survey-form/src/packages/thankYouPage.java
+++ survey-form/src/packages/thankYouPage.java
@@ -13,7 +13,6 @@ public class thankYouPage {
 
         // Create thank you label
         JLabel thankYouLabel = new JLabel("Thanks for your participation!");
-        thankYouLabel.setFont(font);
         thankYouLabel.setBounds(10, 10, 200, 20);
 
         // Add components to the panel
